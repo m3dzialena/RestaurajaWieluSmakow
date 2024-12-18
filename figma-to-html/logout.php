d240ae1c481391e52adbf050e1fd390ac554346b
m3dzialena <120251143+m3dzialena@users.noreply.github.com>
2024-12-18 22:58:22 +0100

    Delete figma-to-html directory

--- figma-to-html/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Usunięcie zmiennych sesji
-session_destroy(); // Zniszczenie sesji
-header("Location: index.php"); // Przekierowanie na stronę główną
-exit();
-?>
